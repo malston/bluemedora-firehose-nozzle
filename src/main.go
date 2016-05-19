cfdf00c3ff546b9b763e1ad55477c5d0b526ce5e
Corbin Phelps <cphelps8907@gmail.com>
2016-05-19 13:57:49 -0400

    Added main function

---
+++ src/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    // "flag"
+    // "os"
+    // "os/signal"
+    // "runtime/pprof"
+    // "syscall"
+    
+)
+
+func main() {
+    
+    //Read in config
+    //Start nozzle
+}
