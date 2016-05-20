fd8d2f2ae8c360975b8e925ff0d6c0fa33e779b1
Corbin Phelps <cphelps8907@gmail.com>
2016-05-20 07:21:22 -0400

    Added logging class

---
+++ logger/logger.go
@@ -0,0 +1,22 @@
+package logger
+
+import (
+    "os"
+    "strings"
+    
+    "github.com/cloudfoundry/gosteno"
+)
+
+func New() *gosteno.Logger {
+    loggingConfig := &gosteno.Config {
+        Sinks:  []gosteno.Sink{
+           gosteno.NewFileSink("./logs/bm_nozzle.log"),  
+        },
+        Level:      gosteno.LOG_DEBUG,
+        Codec:      gosteno.NewJsonCodec(),
+        EnableLOC:  true
+    }
+    
+    gosteno.Init(loggingConfig)
+    return gosteno.newLogger("bm_firehose_nozzle")
+}
