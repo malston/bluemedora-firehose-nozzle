a54ca9d9d46d56213eb41a1452047fe15c28fcf7
Corbin Phelps <cphelps8907@gmail.com>
2016-05-20 08:26:36 -0400

    Have logger working

--- logger/logger.go
+++ logger/logger.go
@@ -1,10 +1,16 @@
 package logger
 
 import (
+    "os"
+    
     "github.com/cloudfoundry/gosteno"
 )
 
+//New logger
 func New() *gosteno.Logger {
+    createLogDirectory()
+    
+    
     loggingConfig := &gosteno.Config {
         Sinks:  []gosteno.Sink{
            gosteno.NewFileSink("./logs/bm_nozzle.log"),  
@@ -16,4 +22,11 @@ func New() *gosteno.Logger {
     
     gosteno.Init(loggingConfig)
     return gosteno.NewLogger("bm_firehose_nozzle")
+}
+
+func createLogDirectory() {
+    if _, err := os.Stat("./logs/"); err == nil {
+        os.RemoveAll("./logs")
+    }
+    os.MkdirAll("./logs/", os.ModePerm)
 }
--- main.go
+++ main.go
@@ -2,12 +2,6 @@ package main
 
 import (
     "flag"
-    "log"
-    "os"
-    // "os"
-    // "os/signal"
-    // "runtime/pprof"
-    // "syscall"
     
     "github.com/BlueMedora/bluemedora-firehose-nozzle/logger"
 )
@@ -16,6 +10,7 @@ func main() {
     flag.Parse()
     
     logger := logger.New()
+    logger.Debug("working log")
     //Read in config
     //Start nozzle
 }
