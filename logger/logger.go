332f37cc61eb06a8f7a6cb035a4211dd6a8189b6
Corbin Phelps <cphelps8907@gmail.com>
2016-08-30 08:36:57 -0400

    Missed a path check for logger

--- logger/logger.go
+++ logger/logger.go
@@ -4,44 +4,50 @@
 package logger
 
 import (
-    "os"
-    "fmt"
-    "path/filepath"
-    "log"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 
-    "github.com/cloudfoundry/gosteno"
+	"github.com/cloudfoundry/gosteno"
 )
 
 //New logger
 func New(logDirectory string, logFile string, loggerName string) *gosteno.Logger {
-    loggingConfig := &gosteno.Config {
-        Sinks:  []gosteno.Sink{
-           gosteno.NewFileSink(fmt.Sprintf("%s/%s", logDirectory, logFile)),  
-        },
-        Level:      gosteno.LOG_DEBUG,
-        Codec:      gosteno.NewJsonCodec(),
-        EnableLOC:  true,
-    }
-    
-    gosteno.Init(loggingConfig)
-    return gosteno.NewLogger(loggerName)
+	loggingConfig := &gosteno.Config{
+		Sinks: []gosteno.Sink{
+			gosteno.NewFileSink(fmt.Sprintf("%s/%s", getAbsolutePath(logDirectory), logFile)),
+		},
+		Level:     gosteno.LOG_DEBUG,
+		Codec:     gosteno.NewJsonCodec(),
+		EnableLOC: true,
+	}
+
+	gosteno.Init(loggingConfig)
+	return gosteno.NewLogger(loggerName)
 }
 
 //CreateLogDirectory clears out old directory and creates a new one
 func CreateLogDirectory(logDirectory string) {
+	absoluteDirectoryPath := getAbsolutePath(logDirectory)
+
+	log.Printf("Using path %s", absoluteDirectoryPath)
+
+	if _, err := os.Stat(fmt.Sprintf("%s/", absoluteDirectoryPath)); err == nil {
+		os.RemoveAll(fmt.Sprintf("%s", absoluteDirectoryPath))
+	}
+
+	os.MkdirAll(fmt.Sprintf("%s/", absoluteDirectoryPath), os.ModePerm)
+}
+
+func getAbsolutePath(logDirectory string) string {
     log.Print("Finding absolute path to log directory")
-    absoluteDirectoryPath, err := filepath.Abs(logDirectory)
+	absolutelogDirectory, err := filepath.Abs(logDirectory)
 
-    if err != nil {
+	if err != nil {
         log.Printf("Error getting absolute path to log directory using relative path due to %v", err)
-        absoluteDirectoryPath = logDirectory
-    }
+		return logDirectory
+	}
 
-    log.Printf("Using path %s", absoluteDirectoryPath)
-
-    if _, err := os.Stat(fmt.Sprintf("%s/", absoluteDirectoryPath)); err == nil {
-        os.RemoveAll(fmt.Sprintf("%s", absoluteDirectoryPath))
-    }
-    
-    os.MkdirAll(fmt.Sprintf("%s/", absoluteDirectoryPath), os.ModePerm)
+	return absolutelogDirectory
 }
